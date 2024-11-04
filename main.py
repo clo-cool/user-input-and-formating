edcf29fd6252e56a582538f6df9d67fd2f3bc358
clo-cool <clodaghhawkes@gmail.com>
2024-11-04 15:31:31 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello WORLD")
+name = "Clodagh"
+print("Hello world.My name is {}".format(name))
+name = input("What is your name ? ")
+age = input("How old are you ? ")
+rang = input("What class are you in ? ")
+teacher = input("How is your teacher ? ")
+teacher2 = input("do you like your teacher ? ")
+print("HI my name is {}. I am {} years old. I am in {} class. My teachers name is {} I think? I {} my teacher".format(name,age,rang,teacher,teacher2))
